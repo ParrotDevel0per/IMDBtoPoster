a40ba485cb2ed2068b44579dfbd7abb4f6fddf0e
ParrotDevelopers <72994504+ParrotDevelopers@users.noreply.github.com>
2022-07-27 23:10:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Docs</title>
+</head>
+<body>
+    <h1 style="color: #5e9ca0;">IMDB to Poster</h1>
+    <h2 style="color: #2e6c80;">Usage:</h2>
+    <p><span style="background-color: #808080; color: #000000;">/poster.php?id=tt8579674</span> - Returns Poster</p>
+    <p><span style="background-color: #808080;">/poster.php?id=tt8579674&amp;do=redirect</span> - Redirects Poster</p>
+    <div class="notranslate" style="all: initial;">&nbsp;</div>
+    <h2 style="color: #2e6c80;">License: Mozilla Public License Version 2.0</h2>
+</body>
+</html>
